0dee4185f9f9897cd9862bb88ca9d7899521e64c
richmond janus rafii aryanto <53498537+Richmondjanusrafiiaryanto@users.noreply.github.com>
2019-08-20 10:50:31 +0700

    first commit

---
+++ src/luas/lingkaran/LuasLingkaran.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package luas.lingkaran;
+
+/**
+ *
+ * @author surga 2019
+ */
+public class LuasLingkaran {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+
+        // Deklarasi
+        Double pi, r, luas;
+        
+        // Deskripsi
+        r = 20.0;
+        pi= 3.14;
+        
+        // Proses
+        luas = pi * r * r;
+        
+        // Output
+        System.out.println("luas lingkaran = ");
+        System.out.println(luas);
+    }
+    
+}
